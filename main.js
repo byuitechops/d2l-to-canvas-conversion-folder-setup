156539669604f34f18a3b41fc5e71a9873d5042f
Seth Childers <sethchilders92@gmail.com>
2018-11-09 11:35:43 -0700

    A module to create the needed folders to run the d2l-conversion-tool

---
+++ main.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+function makeFolders() {
+    const mainDirectoryFolders = [
+        'factory',
+        'reports'
+    ];
+
+    const factoryFolders = [
+        'originalZip',
+        'processed',
+        'unzipped',
+        'uploadZip'
+    ]; 
+
+    // put the needed folders in if they don't already exist
+    mainDirectoryFolders.forEach(folder => {
+        if (!fs.existsSync(path.resolve(folder))) {
+            fs.mkdirSync(path.resolve(folder));
+            console.log(`Made folder: ${folder}`);
+        }
+    });
+
+    factoryFolders.forEach(folder => {
+        if (!fs.existsSync(path.resolve(path.join('factory', folder)))) {
+            fs.mkdirSync(path.resolve(path.join('factory', folder)));
+            console.log(`Made folder: ${folder}`);
+        }
+    });
+}
+
+module.exports = {
+    makeFolders
+};
