8ee452ee85f837f5b516c5c65a1fef9bf26a4739
Danverde <gre12014@byui.edu>
2018-11-12 09:40:56 -0700

    passing params through

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-function makeFolders() {
+module.exports = promptAnswers => {
+    
     const mainDirectoryFolders = [
         'factory',
         'reports'
     ];
-
+        
     const factoryFolders = [
         'originalZip',
         'processed',
         'unzipped',
         'uploadZip'
-    ]; 
-
+    ];
+        
     // put the needed folders in if they don't already exist
     mainDirectoryFolders.forEach(folder => {
         if (!fs.existsSync(path.resolve(folder))) {
@@ -21,15 +22,13 @@ function makeFolders() {
             console.log(`Made folder: ${folder}`);
         }
     });
-
+        
     factoryFolders.forEach(folder => {
         if (!fs.existsSync(path.resolve(path.join('factory', folder)))) {
             fs.mkdirSync(path.resolve(path.join('factory', folder)));
             console.log(`Made folder: ${folder}`);
         }
     });
-}
 
-module.exports = {
-    makeFolders
+    return promptAnswers;
 };
